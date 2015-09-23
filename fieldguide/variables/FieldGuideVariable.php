b24a0c031f1c09dd8e85e063ddd0d6bfc421cd64
Dan Ott <dan@dtott.com>
2015-09-23 14:42:05 -0400

    Add category groups, user groups, asset sources, globals, and settings columns
    This adds full sections for category groups, user groups, asset sources, and global groups.
    
    Also display full settings for each field type, including options for selects/radio/table fields.

--- fieldguide/variables/FieldGuideVariable.php
+++ fieldguide/variables/FieldGuideVariable.php
@@ -3,10 +3,10 @@ namespace Craft;
 
 class FieldGuideVariable {
 
-	public function getBlockTypes($matrixFieldId) 
+	public function getBlockTypes($matrixFieldId)
 	{
 		$blockTypes = craft()->matrix->getBlockTypesByFieldId($matrixFieldId);
-		
+
 		$blockTypesList = array();
 
 		foreach($blockTypes as $blockType) {
@@ -21,7 +21,117 @@ class FieldGuideVariable {
 		return $blockTypesList;
 	}
 
-	public function getUnusedFieldIds() 
+	public function getUserFieldGroup() {
+		return craft()->fields->getLayoutByType('User');
+	}
+
+	public function getUserGroups()
+	{
+		$groups = craft()->userGroups->getAllGroups();
+		$returnGroups = array();
+		foreach ($groups as $group) {
+			$returnGroups[$group->id] = $group;
+		}
+
+		return $returnGroups;
+	}
+
+	public function getCategoryGroups()
+	{
+		$groups = craft()->categories->getAllGroups();
+		$returnGroups = array();
+		foreach ($groups as $group) {
+
+			$returnGroups[$group->id] = array(
+				'handle' => $group->handle,
+				'name' => $group->name,
+				'structureId' => $group->structureId,
+				'fieldLayoutId' => $group->fieldLayoutId
+			);
+		}
+
+		return $returnGroups;
+	}
+
+	public function getFieldLayoutById($id)
+	{
+		return craft()->fields->getLayoutById($id);
+	}
+
+	public function getCategoryGroupBySourceString($string)
+	{
+		$groups = $this->getCategoryGroups();
+		if(strpos($string, ":") !== false) {
+			$string = substr($string, strpos($string, ':') + 1);
+		}
+
+		return $groups[$string];
+	}
+
+	public function getAllAssetSources() {
+		$groups = craft()->assetSources->getAllSources();
+		$returnGroups = array();
+		foreach ($groups as $group) {
+
+			$returnGroups[$group->id] = array(
+				'handle' => $group->handle,
+				'name' => $group->name,
+				'fieldLayoutId' => $group->fieldLayoutId
+			);
+		}
+
+		return $returnGroups;
+
+		// return craft()->assetSources->getAllSources();
+	}
+
+	public function getAllGlobalSets() {
+		$groups = craft()->globals->getAllSets();
+		$returnGroups = array();
+		foreach ($groups as $group) {
+
+			$returnGroups[$group->id] = array(
+				'handle' => $group->handle,
+				'name' => $group->name,
+				'fieldLayoutId' => $group->fieldLayoutId
+			);
+		}
+
+		return $returnGroups;
+	}
+
+	public function getAssetFolderBySourceString($string) {
+
+		if(strpos($string, ":") !== false) {
+			$string = substr($string, strpos($string, ':') + 1);
+		}
+
+		return craft()->assetSources->getSourceById($string)->attributes;
+
+	}
+
+	public function getSectionBySourceString($string) {
+
+		if(strpos($string, ":") !== false) {
+			$string = substr($string, strpos($string, ':') + 1);
+		}
+
+		return craft()->sections->getSectionById($string)->attributes;
+
+	}
+
+	public function getUserGroupBySourceString($string) {
+
+		if(strpos($string, ":") !== false) {
+			$string = substr($string, strpos($string, ':') + 1);
+		}
+
+		return craft()->userGroups->getGroupById($string)->attributes;
+
+	}
+
+
+	public function getUnusedFieldIds()
 	{
 		// all field ids
 		$query = craft()->db->createCommand();
@@ -31,7 +141,7 @@ class FieldGuideVariable {
 			->order('craft_fields.id')
 			->queryAll();
 		$allFieldIds = self::array_flatten($allFieldIds);
-		
+
 		// used field ids
 		$query = craft()->db->createCommand();
 		$query->distinct = true;
@@ -41,13 +151,13 @@ class FieldGuideVariable {
 			->order('craft_fieldlayoutfields.fieldId')
 			->queryAll();
 		$usedFieldIds = self::array_flatten($usedFieldIds);
-		
+
 		// unused field ids
 		$unusedFieldIds = array_diff($allFieldIds, $usedFieldIds);
-		
+
 		return $unusedFieldIds;
 	}
-	
+
 	private function array_flatten($arr) {
 		$arr = array_values($arr);
 		while (list($k,$v)=each($arr)) {
